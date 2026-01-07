f31889da9f44ebfc36039260940a1741805fdb40
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 11:46:19 +0530

    feat: create auth controller structure

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,10 @@
+// Register controller
+const registerController = async (req, res) => {};
+
+// Login controller
+const loginController = async (req, res) => {};
+
+module.exports = {
+  registerController,
+  loginController,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+// importing controllers
+const {
+  registerController,
+  loginController,
+} = require("../controllers/auth.controllers");
+
 // route for user register
-router.post("/register")
+router.post("/register", registerController);
 
 // route for user login
-router.post("/login")
-
+router.post("/login", loginController);
 
 module.exports = router;
