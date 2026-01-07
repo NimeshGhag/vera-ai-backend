8134ee547e35ea2a9ff9391f107b88c4091306cd
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 12:14:29 +0530

    feat: implement user registration with jwt stored in cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,49 @@
+const userModel = require("../models/user.model");
+const JWT = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 // Register controller
-const registerController = async (req, res) => {};
+const registerController = async (req, res) => {
+  const {
+    email,
+    fullName: { firstName, lastName },
+    password,
+  } = req.body;
+
+  const isUser = await userModel.findOne({
+    email,
+  });
+
+  if (isUser) {
+    return res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    email,
+    fullName: {
+      firstName,
+      lastName,
+    },
+    password: hashPassword,
+  });
+
+  const token = JWT.sign({ user: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User Register Successfully",
+    user:{
+        email:user.email,
+        fullName:user.fullName
+    }
+  });
+  
+};
 
 // Login controller
 const loginController = async (req, res) => {};
