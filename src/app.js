971d740733e02f24d38dce51e8a78371e970c725
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 11:42:14 +0530

    feat: setup auth routes for register and login

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+// importing routes
+const authRoutes = require("./routes/auth.routes")
+
 // creating a server
 const app = express();
 
 // Middlwares
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
+app.use("/api/auth",authRoutes)
 
 module.exports = app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+// route for user register
+router.post("/register")
+
+// route for user login
+router.post("/login")
+
+
+module.exports = router;
