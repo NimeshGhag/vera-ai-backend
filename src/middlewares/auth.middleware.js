13d96faab506ae35b20b19265e6f66d9103a2c10
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 12:36:57 +0530

    feat: add jwt auth middleware for protected routes

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const userModel = require("../models/user.model");
+const JWT = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized: No token provided",
+    });
+  }
+
+  try {
+    const decoded = await JWT.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.user);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized: Invalid token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
